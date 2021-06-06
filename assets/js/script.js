609b378492c9499502559515fc0f8d9a63a94db1
Jacob Banks <kobybanks@gmail.com>
2021-06-06 14:41:48 -0400

    added Dropdown tasktype

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,4 +7,5 @@ var createTaskHandler = function () {
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
 };
+
 buttonEl.addEventListener("click", createTaskHandler);
